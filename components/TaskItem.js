9e015eab82f02ea45663d8ecde3b9bac291a9e69
camila-grilletti <camilagrilletti@gmail.com>
2024-10-02 19:53:16 -0300

    fix gesture to delete task

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, StyleSheet, Animated } from 'react-native';
 import { colors, globalStyles } from '../styles/globalStyles';
 import { CheckBox } from '@rneui/themed';
 import MyText from './MyText';
@@ -9,6 +9,7 @@ import Swipeable from 'react-native-gesture-handler/Swipeable';
 
 const TaskItem = ({ task, onDelete }) => {
     const [isChecked, setIsChecked] = useState(false);
+    const translateX = useRef(new Animated.Value(0)).current;
 
     const handleCheckboxPress = () => {
         setIsChecked(!isChecked);
@@ -16,9 +17,19 @@ const TaskItem = ({ task, onDelete }) => {
 
     const textColor = task.tagColor ? getTextColorForBackground(task.tagColor) : null;
 
-    const renderLeftActions = (progress, dragX) => {
-        if (dragX.__getValue() > 100) {
+    const handleSwipeComplete = () => {
+        Animated.timing(translateX, {
+            toValue: -500,
+            duration: 300,
+            useNativeDriver: true,
+        }).start(() => {
             onDelete(task.id);
+        });
+    };
+
+    const renderRightActions = (progress, dragX) => {
+        if (dragX.__getValue() < -100) {
+            handleSwipeComplete();
         }
 
         return (
@@ -28,31 +39,34 @@ const TaskItem = ({ task, onDelete }) => {
 
     return (
         <Swipeable
-            renderLeftActions={renderLeftActions}
-            leftThreshold={100}
+            renderRightActions={renderRightActions}
+            rightThreshold={100}
+            friction={2}
         >
-            <View style={[styles.itemContainer, { backgroundColor: task.color || colors.primary }]}>
-                <CheckBox
-                    checked={isChecked}
-                    onPress={handleCheckboxPress}
-                    uncheckedColor={colors.white}
-                    checkedColor={colors.white}
-                    checkedIcon="check-circle"
-                    uncheckedIcon="circle-o"
-                    containerStyle={styles.checkboxContainer}
-                />
-                <View style={styles.taskTextContainer}>
-                    <MyText style={[styles.taskTextTitle, { color: colors.white }]}>{task.task}</MyText>
-                    {task.tagName && (
-                        <View style={[globalStyles.taskTag, { backgroundColor: task.tagColor }]}>
-                            <MyText style={[{ color: textColor, fontSize: 10 }]}>{task.tagName}</MyText>
-                        </View>
-                    )}
-                    {task.description && (
-                        <MyText style={styles.taskTextDescription}>{task.description}</MyText>
-                    )}
+            <Animated.View style={{ transform: [{ translateX }] }}>
+                <View style={[styles.itemContainer, { backgroundColor: task.color || colors.primary }]}>
+                    <CheckBox
+                        checked={isChecked}
+                        onPress={handleCheckboxPress}
+                        uncheckedColor={colors.white}
+                        checkedColor={colors.white}
+                        checkedIcon="check-circle"
+                        uncheckedIcon="circle-o"
+                        containerStyle={styles.checkboxContainer}
+                    />
+                    <View style={styles.taskTextContainer}>
+                        <MyText style={[styles.taskTextTitle, { color: colors.white }]}>{task.task}</MyText>
+                        {task.tagName && (
+                            <View style={[globalStyles.taskTag, { backgroundColor: task.tagColor }]}>
+                                <MyText style={[{ color: textColor, fontSize: 10 }]}>{task.tagName}</MyText>
+                            </View>
+                        )}
+                        {task.description && (
+                            <MyText style={styles.taskTextDescription}>{task.description}</MyText>
+                        )}
+                    </View>
                 </View>
-            </View>
+            </Animated.View>
         </Swipeable>
     );
 };
